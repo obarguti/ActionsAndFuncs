02d2416575a63e23534465481f20152e7e041566
Barguti <omar.barguti@gmail.com>
2015-04-28 23:24:10 -0400

    initial commit

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace ActionsAndFuncs
+{
+    //public delegate void DoWork();
+    //public delegate void ProcessTask(int duration);
+    //public delegate int DoCalculation(int val1, int val2);
+    //public delegate int DoMoreCalculations(int val1, int val2, int val3);
+    //public delegate bool DoTask();
+
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Func<double> getPI = () => Math.PI;
+            PrintPI(getPI);
+
+            Console.ReadKey();
+        }
+
+        public static void PrintPI(Func<double> func)
+        {
+            Console.WriteLine(func());
+        }
+    }
+}
